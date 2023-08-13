313eb9a960aadee1455dfdde1144c503e1c46601
DhiroxyPlayz <77478271+DhiroxyPlayz@users.noreply.github.com>
2023-08-13 11:21:43 +1000

    Add files via upload

---
+++ Survive XD/main/TBI/shop/store.py
@@ -0,0 +1,20 @@
+from inventory import Inventory
+
+def store():
+    print("")
+    print("Welcome! Do you want to buy anything?")
+    print("")
+    print("(1) House")
+    print("(2) Stone of great power")
+    print("(3) Gun")
+    print("(4) Smart Glasses")
+    print("(5) Pear Mec Book Air")
+    print("(6) Nekia 1")
+    print("(7) Robot")
+    print("(8) Map")
+    print("(9) Bag")
+
+    print("(exit) Leave shop")
+
+    shop = input("What do you want to buy or would u like to leave shop (exit) : ")
+    shop = str(shop)
---
+++ Survive XD/main/surviveXD.py
@@ -0,0 +1,11 @@
+import sys
+
+from TBI.definitions.gameOver import gameOver
+from TBI.definitions.kickedout import kicked_out
+from TBI.definitions.sad import sad
+from TBI.definitions.win import win
+
+from TBI.shop.store import store
+from TBI.shop.inventory import Inventory
+
+
